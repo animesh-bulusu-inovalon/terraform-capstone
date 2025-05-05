ef1ff45476c934543c4134c95d3ea2db0af421b0
Animesh Bulusu <xp.animesh.bulusu@inovalon.global>
2025-05-05 12:56:58 +0530

    Remove vpc and vm layers

--- 03-vm/remote_state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "vpc_main" {
-  backend = "s3"
-  config = {
-    bucket = "animeshbulusu-infrastatefiles-apr2025-02"
-    key    = "02-vpc/02-vpc.tfstate"
-    region = "eu-north-1"
-  }
-}
